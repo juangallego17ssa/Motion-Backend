aa1a63a2ef4edc305cbb8afce2b82231f67217c4
Hatice Anik <haticeanik12@gmail.com>
2023-03-28 16:29:28 +0200

    empty/fix

--- backend/posts/models.py
+++ backend/posts/models.py
@@ -2,7 +2,7 @@ from django.contrib.auth import get_user_model
 from django.db import models
 # from django.contrib.contenttypes.fields import GenericRelation
 # from comments.models import Comment
-# Create your models here.
+# Create your models here
 User = get_user_model()
 
 
