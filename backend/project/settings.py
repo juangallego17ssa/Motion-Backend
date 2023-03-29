d2b082ab7ed79a98393585be448cad6ccc932b3b
juangallego17 <juangallego17@hotmail.com>
2023-03-29 14:55:09 +0200

    corrected url and some settings

--- backend/project/settings.py
+++ backend/project/settings.py
@@ -165,8 +165,15 @@ CSRF_TRUSTED_ORIGINS = ["https://motion-t3.propulsion-learn.ch", 'http://127.0.0
 CORS_ALLOW_ALL_ORIGINS = True
 
 DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL')
-EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS', True)
-EMAIL_HOST = os.environ.get('EMAIL_HOST', "smtp.gmail.com")
-EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', "motionconnectinfo@gmail.com")
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', "lrpjyygjnyeaaten")
-EMAIL_PORT = os.environ.get('EMAIL_PORT', "587")
+EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS')
+EMAIL_HOST = os.environ.get('EMAIL_HOST')
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+EMAIL_PORT = os.environ.get('EMAIL_PORT')
+
+# DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL')
+# EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS', True)
+# EMAIL_HOST = os.environ.get('EMAIL_HOST', "smtp.gmail.com")
+# EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', "motionconnectinfo@gmail.com")
+# EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', "lrpjyygjnyeaaten")
+# EMAIL_PORT = os.environ.get('EMAIL_PORT', "587")
