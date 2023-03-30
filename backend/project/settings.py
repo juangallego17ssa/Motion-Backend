c6123219a7b327cc578a4059501f284de0719c6d
juangallego17 <juangallego17@hotmail.com>
2023-03-30 16:22:58 +0200

    added comment

--- backend/project/settings.py
+++ backend/project/settings.py
@@ -174,6 +174,7 @@ EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 EMAIL_PORT = os.environ.get('EMAIL_PORT')
 
+# Added a comment
 # DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL')
 # EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS', True)
 # EMAIL_HOST = os.environ.get('EMAIL_HOST', "smtp.gmail.com")
